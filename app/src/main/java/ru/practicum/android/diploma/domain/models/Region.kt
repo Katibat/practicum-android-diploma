62a6fe22f4a99fec94130b75e75e9a9d545c2a6b
Milan ISAEV <isaev.milan@gmail.com>
2024-06-18 12:12:43 +0300

    add fragment_region.xml
    -add fragment_region.xml
    -add adapter and holder
    -add region model
    -add city model

---
+++ app/src/main/java/ru/practicum/android/diploma/domain/models/Region.kt
@@ -0,0 +1,13 @@
+package ru.practicum.android.diploma.domain.models
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Region(
+    val id: String,
+    val name: String,
+    val countryId: String?,
+    val cities: List<City>
+) : Parcelable
+
